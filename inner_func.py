81919e8278b4513f05428f76045da9fbf7745897
Abu Naim <abunaim@Abus-MacBook-Air-2.local>
2023-11-23 04:11:47 +0600

    inner function

--- inner_func.py
+++ inner_func.py
@@ -1,4 +1,7 @@
 # In python function is a first class object
+
+# inner function :
+
 def double_dacker():
     print('Starting the double dacker')
     def inner():
@@ -7,3 +10,24 @@ def double_dacker():
     return inner
     
 print(double_dacker()())
+
+# take a function as parameter 
+
+def do_something(work):
+    print('Work Start')
+    # print(work)
+    work()
+    print('Work Ended')
+
+do_something(2)
+do_something('This is naim')
+
+def coding():
+    print('Love coding')
+
+do_something(coding)
+
+def sleeping():
+    print('Sleeping and dreaming in python')
+
+do_something (sleeping)
