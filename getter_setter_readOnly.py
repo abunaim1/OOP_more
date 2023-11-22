e46429f88350f369e31778e9b2df7aafaf445381
Abu Naim <abunaim@Abus-MacBook-Air-2.local>
2023-11-23 03:26:13 +0600

    getter setter learn

---
+++ getter_setter_readOnly.py
@@ -0,0 +1,31 @@
+class User:
+    def __init__(self, name, age, money) -> None:
+        self._name = name
+        self._age = age
+        self.__money = money
+    
+    #getter without any setter is ready-only attribute 
+    @property
+    def age(self):
+        return self._age
+    
+    #getter
+    @property
+    def salary(self):
+        return self.__money
+    
+    # getter must then setter
+    # setter
+    @salary.setter
+    def salary(self, value):
+        if value < 0:
+            return 'Money can not be neg.' 
+        self.__money += value
+
+
+user1 = User('Naim', 22, 4000)
+print(user1.salary) # not using method though salary is being used as a method avobe. 
+
+user1.salary = 1000
+print(user1.salary)
+
