5fcf0d27f912548983104e3f7df2ef5d9d565ff3
Abu Naim <abunaim@Abus-MacBook-Air-2.local>
2023-11-26 11:39:53 +0600

    composition

---
+++ composition.py
@@ -0,0 +1,20 @@
+class Engine:
+    def __init__(self) -> None:
+        pass
+    def start(self):
+        return "engine started"
+
+class Driver:
+    def __init__(self) -> None:
+        pass
+
+
+# car "Has a" engine
+# car "has a" driver
+class Car:
+    def __init__(self) -> None:
+        self.engine = Engine()
+        self.driver = Driver()
+        
+    def start(self):
+        self.engine.start()
---
+++ inheritance.py
@@ -0,0 +1,19 @@
+# inheritance provides me "is a" relation.
+
+class Animal:
+    pass
+
+#Tiger "is a" Animal
+class Tiger(Animal):
+    pass
+
+class Furniture:
+    pass
+
+#tabel "is a" furniture
+class Table(Furniture):
+    pass
+
+#bed "is a" furniture
+class Bed(Furniture):
+    pass
